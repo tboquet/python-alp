ccd0a0c8cb5f6335414e6e005a5875caf703a390
tboquet <thomas.boquet@hec.ca>
2016-04-14 15:02:14 -0400

    + keras backend and tests

---
+++ src/alp/backend/keras_backend.py
@@ -0,0 +1,70 @@
+"""Adaptor for the Keras backend"""
+
+from keras.models import model_from_json
+import keras.backend as K
+
+
+def build_predict_func(mod):
+    """Build Keras prediction functions based on a Keras model
+
+    Using inputs and outputs of the graph a prediction function
+    (forward pass) is compiled for prediction purpose.
+
+    Args:
+        mod(keras.models): a Graph or Sequential model
+
+    Returns:
+        a Keras (Theano or Tensorflow) function
+    """
+    # get inputs
+    inputs = [mod.inputs[inp].input for inp in mod.input_order]
+
+    # get outputs
+    outputs = []
+    for out in mod.output_order:
+        outputs.append(mod.outputs[out].get_output(train=False))
+
+    return K.function(inputs, outputs, updates=mod.state_updates)
+
+
+def train_model(model_str, custom_objects, datas, datas_val, batch_size,
+                nb_epoch, callbacks):
+    """Train a model given hyperparameters and a serialized model"""
+
+    loss = []
+    val_loss = []
+    # load model
+    model = model_from_json(model_str, custom_objects=custom_objects)
+
+    # fit the model according to the input/output type
+    if model.name == 'Graph':
+        for d, dv in zip(datas, datas_val):
+            h = model.fit(data=d,
+                            batch_size=batch_size,
+                            nb_epoch=nb_epoch,
+                            verbose=1,
+                            callbacks=callbacks,
+                            validation_data=dv)
+            loss += h.history['loss']
+            if 'val_loss' in h.history:
+                val_loss += h.history['val_loss']
+    elif model.name == 'Sequential':
+        # unpack data
+        for d, dv in zip(datas, datas_val):
+            X, y = d['X'], d['y']
+            X_val, y_val = dv['X'], dv['y']
+            h = model.fit(X=X,
+                            y=y,
+                            batch_size=batch_size,
+                            nb_epoch=nb_epoch,
+                            verbose=1,
+                            callbacks=callbacks,
+                            validation_data=(X_val, y_val))
+            loss += h.history['loss']
+            if 'val_loss' in h.history:
+                val_loss += h.history['val_loss']
+    else:
+        raise NotImplementedError('This type of model is not supported')
+
+    return loss, val_loss, model
+
---
+++ tests/backend/test_keras.py
@@ -0,0 +1,74 @@
+"""Tests for the Keras backend"""
+import pytest
+import os
+import sys
+import numpy as np
+np.random.seed(1337)
+
+from keras import callbacks
+from keras.models import Graph, Sequential
+from keras.layers.core import Dense
+from keras.utils.test_utils import get_test_data
+from keras import backend as K
+from keras.utils import np_utils
+
+
+input_dim = 2
+nb_hidden = 4
+nb_class = 2
+batch_size = 5
+train_samples = 20
+test_samples = 20
+
+
+def test_build_predict_func():
+    """Test the build of a model"""
+    (X_tr, y_tr), (X_te, y_te) = get_test_data(nb_train=train_samples,
+                                               nb_test=test_samples,
+                                               input_shape=(input_dim,),
+                                               classification=True,
+                                               nb_class=nb_class)
+
+    y_test = np_utils.to_categorical(y_test)
+    y_train = np_utils.to_categorical(y_train)
+
+    model = Sequential()
+    model.add(Dense(nb_hidden, input_dim=input_dim, activation='relu'))
+    model.add(Dense(nb_class, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    pred_func = build_predict_func(model)
+    res = pred_func([X_test])
+
+    assert 0 == 0
+
+
+def test_train_model():
+    "Test the training of a serialized model"
+    (X_tr, y_tr), (X_te, y_te) = get_test_data(nb_train=train_samples,
+                                               nb_test=test_samples,
+                                               input_shape=(input_dim,),
+                                               classification=True,
+                                               nb_class=nb_class)
+    datas, datas_val = dict(), dict()
+
+    datas["X"] = X_tr
+    datas["y"] = y_tr
+
+    datas_val["X"] = X_tr
+    datas_val["y"] = y_tr
+
+    model = Sequential()
+    model.add(Dense(nb_hidden, input_dim=input_dim, activation='relu'))
+    model.add(Dense(nb_class, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    model_str = model.to_json()
+    res = train_model(model_str, [], datas, datas_val, batch_size,
+                      2, [])
+
+    assert 0 == 0
