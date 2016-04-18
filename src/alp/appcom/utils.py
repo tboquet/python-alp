e717544de673d8cc7cf269ca8915fbec2e008f17
tboquet <thomas.boquet@hec.ca>
2016-04-18 18:26:12 -0400

    + utils and tests

---
+++ src/alp/appcom/utils.py
@@ -0,0 +1,31 @@
+"""Utility functions for the appcom module
+
+----------------------------------------------------------------------------
+"""
+from functools import wraps
+
+
+def sliced(data, nb_train, nb_test, offset):
+    """Given a dataset, returns indexes to split the data into
+    a train and a validation set.
+
+    Args:
+        data(dict): a dictionnary mapping names to np.arrays
+        nb_train(int): the number of train samples
+        nb_test(int): the number of test samples
+        offset(int): the first observation offset
+
+    Returns:
+        `beg`, `endt`, `endv`, the indexes corresponding to
+         the beginning, the end of training end the end of
+         testing."""
+    first = data.keys()[0]
+    assert len(data[first]) > nb_train + nb_test + offset, \
+        'nb or nb + offset too large:' \
+        ' len(data):{}' \
+        ', len(selection): {}'.format(len(data[first]),
+                                  nb_train + nb_test + offset)
+    beg = offset - len(data[first])
+    endt = beg + nb_train
+    endv = endt + nb_test
+    return beg, endt, endv
---
+++ tests/appcom/test_appcom.py
@@ -0,0 +1,16 @@
+"""Tests for the appcom module"""
+
+import numpy as np
+import pytest
+from alp.appcom.utils import sliced
+
+def test_sliced():
+    data = {str(k): np.ones((100,2)) for k in range(10)}
+    indices = sliced(data, 30, 20, 10)
+    assert length(data[:indices[0]]) == 10
+    assert length(data[indices[0]:indices[1]]) == 30
+    assert length(data[indices[1]:indices[2]]) == 20
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
