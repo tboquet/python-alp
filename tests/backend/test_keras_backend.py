59715a3f0e94555b413f1c803a47ccd44d8dd4bc
tboquet <thomas.boquet@hec.ca>
2016-04-18 13:36:32 -0400

    * isort

--- tests/backend/test_keras_backend.py
+++ tests/backend/test_keras_backend.py
@@ -9,8 +9,8 @@ from keras.utils import np_utils
 from keras.utils.test_utils import get_test_data
 
 from alp.backend import keras_backend as KTB
-from alp.backend.utils.keras_utils import to_dict_w_opt
 from alp.backend.utils.keras_utils import get_function_name
+from alp.backend.utils.keras_utils import to_dict_w_opt
 
 np.random.seed(1337)
 
