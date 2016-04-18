c9d1e1548250bbef73759dc8807d4ad36d18af55
tboquet <thomas.boquet@hec.ca>
2016-04-18 18:46:20 -0400

    * R reflex

--- tests/appcom/test_appcom.py
+++ tests/appcom/test_appcom.py
@@ -7,9 +7,9 @@ from alp.appcom.utils import sliced
 def test_sliced():
     data = {k: np.ones((100,2)) for k in range(10)}
     indices = sliced(data, 30, 20, 10)
-    assert length(data[1][:indices[0]]) == 10
-    assert length(data[1][indices[0]:indices[1]]) == 30
-    assert length(data[1][indices[1]:indices[2]]) == 20
+    assert len(data[1][:indices[0]]) == 10
+    assert len(data[1][indices[0]:indices[1]]) == 30
+    assert len(data[1][indices[1]:indices[2]]) == 20
 
 
 if __name__ == "__main__":
