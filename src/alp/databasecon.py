3ab33b19af3268cb27eb5f92dd7e9288dea06eb8
tboquet <thomas.boquet@hec.ca>
2016-04-21 18:11:55 -0400

    + databasecon

---
+++ src/alp/databasecon.py
@@ -0,0 +1,21 @@
+"""Model database setup
+
+----------------------------------------------------------------------------
+"""
+
+import os
+from pymongo import MongoClient
+from config import COLLECTION_NAME
+from config import DB_NAME
+from config import HOST_ADRESS
+from config import HOST_PORT
+
+
+def get_models():
+    """Utility function to retrieve the collection of models
+
+    Returns:
+        the collection of models"""
+    client = MongoClient(HOST_ADRESS, HOST_PORT)
+    modelization = client[DB_NAME]
+    return modelization[COLLECTION_NAME]
