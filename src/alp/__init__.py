c98bc703a3efc2bbca63251188896c56bdc699e6
tboquet <thomas.boquet@hec.ca>
2016-06-08 12:00:43 -0400

    * use from .

--- src/alp/__init__.py
+++ src/alp/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from .appcom import *
+from . import appcom
+from . import dbbackend
 
-__all__ = ["Experiment"]
+__all__ = ["Experiment", "appcom", "dbbackend"]
 
 __version__ = "0.2.0"
