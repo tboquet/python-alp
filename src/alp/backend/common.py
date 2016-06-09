7918229ab9be158ce0e8fca634faa566192bed5c
tboquet <thomas.boquet@hec.ca>
2016-06-09 12:49:18 -0400

    * use enumerate instead of range(len())

--- src/alp/backend/common.py
+++ src/alp/backend/common.py
@@ -16,7 +16,7 @@ def create_model_hash(model, batch_size):
 def create_data_hash(data):
     un_data_m = 0
     un_data_f = 0
-    for i in range(len(data)):
+    for i, _ in enumerate(data):
         for key in data[i]:
             un_data_m += data[i][key].mean()
             un_data_f += data[i][key][0]
