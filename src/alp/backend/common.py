13c242e99a868514bc57c64234b162ad074dc22a
tboquet <thomas.boquet@hec.ca>
2016-10-18 18:14:08 -0400

    * no cover on indexes

--- src/alp/backend/common.py
+++ src/alp/backend/common.py
@@ -154,7 +154,7 @@ def transform_gen(gen_train, mod_name):
                         list_outputs = True
                     else:
                         outputs_dict[name[7:]] = arr
-                elif 'index' in name:
+                elif 'index' in name:  # pragma: no cover
                     pass
                 else:  # pragma: no cover
                     raise("Not input nor output, please check your generator")
