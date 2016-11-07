4bd37bb9204d76a68332d3c2ea5d58fea3b34df9
tboquet <thomas.boquet@hec.ca>
2016-11-07 10:15:33 -0500

    * serializer

--- src/alp/celapp.py
+++ src/alp/celapp.py
@@ -6,3 +6,7 @@ from . import appcom as apc
 
 app = Celery(broker=apc._broker,
              backend=apc._backend)
+
+app.conf.update(task_serializer='pickle',
+                result_serializer='pickle',
+                accept_content=['pickle', 'json'])
